12ea8263f7312164b24f5e39376fd356911478ed
Joe <jgacheru504@gmail.com>
2025-03-12 12:08:15 +0300

    css animations
    css animations

--- index.js
+++ index.js
@@ -1,52 +1,16 @@
-let menu = document.querySelector('.menu')
-let nav = document.querySelector('.nav')
+let menuBar = document.querySelector('.menuBar')
 let body = document.querySelector('body')
-let logo = document.querySelector('.logo')
-let  = document.querySelector('body')
+let contactBtn =document.querySelector('.contactBtn')
 
-function showMenu() {
-    if ( nav.style.display == 'block'){
-        nav.style.display ='none';
-        logo.style.background = 'white'
-        nav.style.background = 'red'
-    }
-    else{
-        nav.style.display ='block';
-        logo.style.background = 'red'
-        nav.style.background = 'red'
-    }
-}
+contactBtn.addEventListener('click',function(){
+    alert('0706571416') 
+})
 
-menu.addEventListener('click', showMenu)
+let nav = document.querySelector('.nav')
+let navBar = document.querySelector('.navbar')
 
-let  content = document.querySelector('.content')
-let button = document.querySelector('button')
-
-function showContent() {
-    if ( content.style.display == 'block'){
-        content.style.display ='none';
-        
-    }
-    else{
-        content.style.display ='block';
-       
-    }
-}
-
-function showContent() {
-    content.style.display == 'block' ? content.style.display ='none' : content.style.display ='block'
-}
-
-button.addEventListener('click', showContent)
-
-
-let faq = document.getElementsByClassName('container')
-
-for (i=0; i < faq.length; i++){
-    faq[i].addEventListener('click', function(){
-        this.classList.toggle('active')
-    })
-}
-
-
+menuBar.addEventListener('click', function(){    
+    nav.style.display == 'block' ? nav.style.display = 'none':nav.style.display = 'block'; 
+    document.body.classList.toggle('no-scroll');
+})
 
